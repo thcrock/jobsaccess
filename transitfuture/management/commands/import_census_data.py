a1bbf6fc3388b3a393cf8b1404907a9659db561d
Tristan Crockett <tristan@edgeflip.com>
2014-11-11 23:56:12 -0600

    Import census data

---
+++ transitfuture/management/commands/import_census_data.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from urllib import urlopen
+import gzip
+from StringIO import StringIO
+import csv
+from transitfuture.models import CensusBlock
+
+class Command(BaseCommand):
+    help = 'Imports US Census LODES data'
+
+
+
+    def handle(self, *args, **options):
+        for segment in ('S000', 'SA01', 'SA02', 'SA03', 'SE01', 'SE02', 'SE03', 'SI01', 'SI02', 'SI03'):
+            url = urlopen("http://lehd.ces.census.gov/data/lodes/LODES7/il/wac/il_wac_{}_JT00_2011.csv.gz".format(segment))
+            s = StringIO(url.read())
+            with gzip.GzipFile(fileobj=s) as f:
+                reader = csv.reader(f)
+                header_row = reader.next()
+                header = {}
+                for (i, field) in enumerate(header_row):
+                    header[i] = field
+                for row in reader:
+                    atts = dict((header[i], value) for (i, value) in enumerate(row))
+                    atts['workforce_segment'] = segment
+                    atts['census_block'] = atts['w_geocode']
+                    del atts['w_geocode']
+                    del atts['createdate']
+                    obj = CensusBlock(**atts)
+                    obj.save()
